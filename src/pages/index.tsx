5bc99058b3e7a7140f406b9bba11a3e6c80e8fca
amrik malhans <amrikdeveloper@gmail.com>
2022-12-25 18:47:18 -0800

    update work exp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,28 +19,35 @@ const Index = () => {
 				<Title size={3}>Hey, I&apos;m Amrik Malhans! </Title>
 			</Center>
 			<Paragraph>
-				I&apos;m a Backend Engineer and I utilise tech like TypeScript, Go,
-				Node.JS, GraphQL and PostgreSQL to build APIs that scale. I also hold
-				experience with Web3 and Blockchain technologies.
+				I&apos;m a Backend Engineer building scalable and performant
+				applications using{" "}
+				<TextGradient>
+					<Heavy>Node.JS, TypeScript, Go, and PostgreSQL. </Heavy>
+				</TextGradient>
+				I also have experience with{" "}
+				<TextGradient>
+					<Heavy>React, Next.JS, and GraphQL </Heavy>
+				</TextGradient>
+				to build full stack applications.
 			</Paragraph>
 			<Paragraph>
-				I&apos;m currently a Backend Engineer at{" "}
-				<StyledLink target="_blank" href="https://cryptokek.com/">
-					Cryptokek{" "}
+				I&apos;m currently an Engineer at{" "}
+				<StyledLink target="_blank" href="https://brookbushinstitute.com/">
+					Brookbush Institute{" "}
 				</StyledLink>
 				working with{" "}
 				<TextGradient>
-					<Heavy>Node.JS + TypeScript + Web3 </Heavy>
+					<Heavy>Next.JS + Node.JS + TypeScript</Heavy>
 				</TextGradient>
-				to build a token and chain explorer.
+				to build the next generation of online education.
 			</Paragraph>
 
 			<Paragraph>
 				I also enjoy a lot of other things, in no particular order:
 				<br />
 				<br />
-				Philosophy, Books, Mechanical Keyboards, Crypto, Metal Music, and
-				soccer!
+				Nietzsche&apos;s philosophy, mind expanding books, hacking productivity,
+				outdoor adventures, exploring new cultures, and learning new things.
 			</Paragraph>
 		</Fragment>
 	);
